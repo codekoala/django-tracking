9ee19ee47a76be04e479c88cc8a7284f23c78983
Igor Shevchenko <phar0s@ya.ru>
2013-03-22 23:39:24 +0600

    Register Visitor and SearchItem models in admin.py with list_display

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from tracking.models import BannedIP, UntrackedUserAgent
+from tracking.models import BannedIP, UntrackedUserAgent, Visitor, SearchItem
+
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ('session_key', 'ip_address', 'user_agent', 'referrer',
+                    'url', 'page_views', 'session_start', 'last_update')
+
+class SearchItemAdmin(admin.ModelAdmin):
+    list_display = ('engine', 'query', 'visitor')
 
 admin.site.register(BannedIP)
 admin.site.register(UntrackedUserAgent)
+admin.site.register(Visitor, VisitorAdmin)
+admin.site.register(SearchItem, SearchItemAdmin)
