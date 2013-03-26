60b8bca1a6a989afb30b031d5ae075f4ba1748f1
Igor Shevchenko <phar0s@ya.ru>
2013-03-26 15:13:49 +0600

    Register site object in admin

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from tracking.models import BannedIP, UntrackedUserAgent, Visitor, SearchItem
+from tracking.models import BannedIP, UntrackedUserAgent, Visitor, SearchItem, SiteObject
+
+def visitor_session(obj):
+    return obj.visitor.session_key
 
 class VisitorAdmin(admin.ModelAdmin):
     list_display = ('session_key', 'ip_address', 'user_agent', 'referrer',
                     'url', 'page_views', 'session_start', 'last_update')
 
 class SearchItemAdmin(admin.ModelAdmin):
-    list_display = ('engine', 'query', 'visitor')
+    list_display = ('engine', 'query', visitor_session)
+
+class SiteObjectAdmin(admin.ModelAdmin):
+    list_display = ('content_object', visitor_session)
 
 admin.site.register(BannedIP)
 admin.site.register(UntrackedUserAgent)
 admin.site.register(Visitor, VisitorAdmin)
 admin.site.register(SearchItem, SearchItemAdmin)
+admin.site.register(SiteObject, SiteObjectAdmin)
